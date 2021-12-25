cc44381c2d8d1027d7765cbc7d0e65343002bed6
somu0 <94071371+somu0@users.noreply.github.com>
2021-12-25 17:39:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+num1=10
+num2=20
+num3=15
+ if (num1<=num2&&num1<=num3){
+  console.log("num1 is a minimum number");
+}else if(num2<=num3){
+  console.log("num2 is a minimum number");
+}else{
+  console.log("num3 is a minimum number");
+}
